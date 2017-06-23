55ef9e119fa17ddbb4978f2e9a8ec0e4a57b2f16
Benjamin Jordan <benjamin@createar.co>
2017-06-23 16:14:11 -0700

    Initial commit.

---
+++ ILogFormatter.cs
@@ -0,0 +1,17 @@
+﻿namespace CreateAR.Commons.Logging
+{
+    /// <summary>
+    /// An interface for formatting logs.
+    /// </summary>
+    public interface ILogFormatter
+    {
+        /// <summary>
+        /// Formats a log.
+        /// </summary>
+        /// <param name="level">The level of the log to be formatted.</param>
+        /// <param name="caller">The calling object.</param>
+        /// <param name="message">The message to log.</param>
+        /// <returns></returns>
+        string Format(LogLevel level, object caller, string message);
+    }
+}
